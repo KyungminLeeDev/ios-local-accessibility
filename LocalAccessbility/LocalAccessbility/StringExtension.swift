74ac941b2ce7e72438e3c69ee95ad55527af40f3
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-03-25 15:09:33 +0900

    연산 프로퍼티로 변경

--- LocalAccessbility/LocalAccessbility/StringExtension.swift
+++ LocalAccessbility/LocalAccessbility/StringExtension.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension String {
-    func localized(bundle: Bundle = .main, tableName: String = "Localizable") -> String {
-        return NSLocalizedString(self, tableName: tableName, value: "**\(self)**", comment: "")
+    var localized: String {
+        return NSLocalizedString(self, tableName: "Localizable", value: "self", comment: "")
     }
 }
--- LocalAccessbility/LocalAccessbility/ViewController.swift
+++ LocalAccessbility/LocalAccessbility/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         let bottomLabel = UILabel()
         bottomLabel.frame = CGRect(x: 20, y: view.bounds.size.height - 150, width: view.bounds.size.width - 40, height: 50)
         
-        bottomLabel.text = "Touch the button below!".localized()
+        bottomLabel.text = "Touch the button below!".localized
         
         bottomLabel.font = UIFont.systemFont(ofSize: 20)
         
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
         let button = UIButton(type: .system)
         button.frame = CGRect(x: 20, y: view.bounds.size.height - 80, width: view.bounds.size.width - 40, height: 50)
         
-        button.setTitle("Touch Me!".localized(), for: .normal)
+        button.setTitle("Touch Me!".localized, for: .normal)
         
         
         button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
